3ec5e4c42a48e033b52fe1931fe8584f3f246342
captainSolo <captainsolo23@gmail.com>
2023-07-24 15:03:50 -0700

    jwt protect

--- backend/routes/usersRoutes.js
+++ backend/routes/usersRoutes.js
@@ -3,11 +3,13 @@ const router = express.Router()
 
 const {registerUser, loginUser, getUserData } = require('../controlers/usersControllers.js')
 
+const { protect } = require ('../middleware/AuthMiddleware')
+
 //rutas publicas
 router.post('/', registerUser)
 router.post('/login', loginUser)
 
 //ruta privada
-router.get('/getMe', getUserData)
+router.get('/getMe', protect, getUserData)
 
 module.exports = router
